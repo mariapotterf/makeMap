e262a645a2745b3985e0b4428376c8dd73b5b283
Potterf <mpotterf@jyu.fi>
2021-02-05 23:44:06 +0200

    complated example with tmap to plot raster and polygon data

--- Gradient_study_site.R
+++ Gradient_study_site.R
@@ -19,8 +19,8 @@ library(ggplot2)
 library(patchwork)     # For multiple map layout
 library(ggsn)          # add scale, north arrow, works with ggplot2
 #library(ggmap)         # add scale, north arrow...
-#library(ggspatial)     # add static map elements: scale, north arror
-
+library(ggspatial)     # add static map elements: scale, north arror
+library(stars)
 
 
 # Get input data
@@ -32,22 +32,41 @@ europe <- st_read(paste(getwd(), "Europe_ERTS.shp", sep = "/"))  # polygon
 site   <- st_read(paste(getwd(), "pts_bioregions.shp", sep = "/")) # point
 forest <- raster(paste(getwd(), "eea_r_3035_100_m_forest-area-2015_p_2015_v1_r1.tif", sep = "/"))
 
+#cropped raster
+forest <- raster(paste(getwd(), "forest_simple2_int_rcl.tif", sep = "/"))
+
+
+
+
+# CHeck coordinate system of raste and polygons ---------------------------
+
+crs(site) <- crs(forest)
+crs(europe)
+
+
 # Chcek attributes = columns
 names(site)
 
 
+# Check raster attributes - it is too big, need to change it to plot in ggplot2
+hist(values(forest))
 
 
 # Process data ------------------------------------------------------------
 
 # subset FI, GE and NO to add them as new layer
 countries = europe %>% filter(NAME == "Finland"| NAME == "Norway"|NAME == "Germany")
-#no = europe %>% filter(NAME == "Norway")
-#ge = europe %>% filter(NAME == "Germany")
+
+# Convert sf object to start object
+eu_star <- st_as_stars(europe)
 
 
 # project raster data
 st_crs(europe)
+
+# Convert raster to polygon
+#r.to.poly <- sf::as_Spatial(sf::st_as_sf(stars::st_as_stars(forest), 
+     #                                    as_points = FALSE, merge = TRUE))
 
 
 # Reclassify bioblimatic regions for NO, FI, GE
@@ -69,7 +88,7 @@ site <- site %>%
 
 # Plot data
 # will plot all columns that data hs, need to plot only geomtry
-plot(europe["NAME"], fill = "grey")
+#plot(europe["NAME"], fill = "grey")
 
 
 # Plot in ggplot2
@@ -87,36 +106,75 @@ ggplot(data = europe) +
  # geom_raster(data = forest) + 
   #coord_quickmap()
 
+# Reclassify raster values: now has 0,1,65535 which is NO data, only 1 = forest
+#m <- c(0, 0.25, 1,  0.25, 0.5, 2,  0.5, 1, 3)
+#rclmat <- matrix(m, ncol=3, byrow=TRUE)
 
+#forest[forest == 65535] <- NA
+#forest[forest == 0]     <- NA
+# get object size
+#object.size(forest)
 
+# INcrease memory size?
+# Check memory limit: memory.limit() 
+# memory.limit(size = 35000) 
 
-
+# clean garbage in r: gc()
 
 # Make a map -----------------------------------------
 # example on ggplot2 & inset map: https://upgo.lab.mcgill.ca/2019/12/13/making-beautiful-maps/
 
+
+# Get color blind palete
+# The palette with black:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+
+
 windows()
 p1 <- ggplot(data = europe) +
   geom_sf(color = 'black',
-          fill = 'lightgrey') +
-  xlab("Longitude") + ylab("Latitude") +
+          fill = 'lightgrey',
+          size = 0.3) +
+  xlab("Longitude") + 
+  ylab("Latitude") +
   ggtitle("Europe", 
           subtitle = paste0("(", length(unique(europe$NAME)), " countries)")) +
-  # get limits for the x and y axis - guess them from teh long (x) and lat (y)
-  # we are on the east and nort so both values are +
-  
-  # geom_sf(data = countries, 
-  #        fill = "grey10",
-  #        color = 'grey10') +
   geom_sf(data = site, 
           aes(color = bioclim)) +
+  scale_colour_manual(values=cbbPalette) +
+  scale_shape_manual(values=c(1,2,3)) +
+  #scale_color_brewer(palette="Dark2") + 
   # This boundary settings is mostly rial and error process
   coord_sf(xlim = c(-1000000, 2000000),
            ylim = c( 5000000,  7800000)) + # set zoom as the last one as I am adding new shp
-#ylim = c(45, 72), expand = FALSE
+  #ylim = c(45, 72), expand = FALSE
   # theme_void() # # Set a completely blank theme, to get rid of all background and axis elements
   theme_bw() #+
  # ggspatial::annotation_north_arrow()
+
+
+
+# Check out ggspatial library ---------------------------------------------
+
+
+
+p1 + 
+  annotation_scale(location = "br", 
+                   width_hint = 0.4,
+                   style = "bar") +  # or style = "ticks"
+  annotation_north_arrow(location = "bl", 
+                         which_north = "false", 
+                         pad_x = unit(3.6, "in"), 
+                         pad_y = unit(2.9, "in"),
+                         style = north_arrow_fancy_orienteering)
+  
+  
+  
+
+
+
+# Check out ggsn library --------------------------------------------------
 
 
 p1 +# blank() +
@@ -127,16 +185,78 @@ p1 +# blank() +
               #location = 'topright',
               scale = 0.2, #  # scale: 0-1, proportion of teh size
               symbol = 3)  # get symbols by northSymbols()
-  ggsn::scalebar(data = europe, 
-                 dist = 100, 
-                 dist_unit = "km",
-           transform = FALSE)
+ggsn::scalebar(data = europe, 
+               dist = 100, 
+               dist_unit = "km",
+               transform = FALSE)
 
 
 
-  
-  
-  
+# Plot vector over raster -------------------------------------------------
+
+
+
+
+
+# Get started with tmaps  -------------------------------------------------
+
+# https://cran.r-project.org/web/packages/tmap/vignettes/tmap-getstarted.html#multiple-shapes-and-layers
+# Plot raster data in tmap?
+
+# Shape in tmap is spatial object (class: sf, sp. stars, or raster)
+
+library(stars)
+
+library(tmap)
+
+g = st_graticule(europe, 
+                 lon = c(-10, 20), 
+                 lat = c( 50,  78))
+
+windows()
+
+#eu_map <- 
+  tm_shape(europe) +
+    tm_polygons() +
+  #tm_shape(forest) +
+ # tm_raster(alpha = 0.9) +
+  tm_shape(site) +
+    tm_dots() +
+  tm_compass(type = "2star", position = c("left", "top")) +
+  tm_scale_bar(breaks = c(0, 100, 200), text.size = 1)
+
+
+plot(eu_map, graticule = g, axes = TRUE, col = "blue")
+
+
+
+
+
+data("World")
+
+tm_shape(World) +
+  tm_polygons("gdp_cap_est")
+
+
+# Make interactive mode of maps:
+
+tmap_mode("view")
+
+
+tm_shape(World) +
+  tm_polygons("HPI")
+
+
+# end inteactive mode; mode is to plotting
+ttm()
+tmap_mode("plot")
+
+
+
+
+tm_shape(europe) +
+  tm_fill() 
+
   
   
   # Example to use ggsn package ---------------------------------------------
@@ -181,3 +301,54 @@ p1 + ggspatial::annotation_scale(
       text_family = "ArcherPro Book"
     )
   )
+
+
+
+
+
+
+
+#  Example thematic map raster --------------------------------------------
+
+# https://rdrr.io/cran/tmap/man/tm_raster.html
+
+pal8 <- c("#33A02C", "#B2DF8A", "#FDBF6F", "#1F78B4", "#999999", "#E31A1C", "#E6E6E6", "#A6CEE3")
+
+
+tm_shape(land, ylim = c(-88,88)) +
+  tm_raster("cover_cls", palette = pal8, title = "Global Land Cover") +
+  tm_shape(metro) + tm_dots(col = "#E31A1C") +
+  tm_shape(World) +
+  tm_borders(col = "black") +
+  tm_layout(scale = .8, 
+            legend.position = c("left","bottom"),
+            legend.bg.color = "white", legend.bg.alpha = .2, 
+            legend.frame = "gray50")
+
+## Not run: 
+pal20 <- c("#003200", "#3C9600", "#006E00", "#556E19", "#00C800", "#8CBE8C",
+           "#467864", "#B4E664", "#9BC832", "#EBFF64", "#F06432", "#9132E6",
+           "#E664E6", "#9B82E6", "#B4FEF0", "#646464", "#C8C8C8", "#FF0000",
+           "#FFFFFF", "#5ADCDC")
+tm_shape(land) +
+  tm_raster("cover", palette = pal20, title = "Global Land Cover") + 
+  tm_layout(scale=.8, legend.position = c("left","bottom"))
+
+## End(Not run)
+
+
+
+tm_shape(land, ylim = c(-88,88)) +
+  tm_raster("trees", palette = "Greens", title = "Percent Tree Cover") +
+  tm_shape(World) +
+  tm_borders() +
+  tm_layout(legend.position = c("left", "bottom"), bg.color = "lightblue")
+
+## Not run: 
+tm_shape(land) +
+  tm_raster("black") +
+  tm_facets(by="cover_cls")
+
+## End(Not run)
+
+# TIP: check out these examples in view mode, enabled with tmap_mode("view")
