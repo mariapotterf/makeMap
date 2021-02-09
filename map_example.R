10038e44c0a77d34e0dd0061500f7b542c16cfde
Potterf <mpotterf@jyu.fi>
2021-02-09 21:57:15 +0200

    completed examples

--- map_example.R
+++ map_example.R
@@ -73,3 +73,88 @@ map_nz +
   tm_scale_bar(breaks = c(0, 100, 200, 300), text.size = 1)
 
 
+
+
+# Example to use ggsn package ---------------------------------------------
+# http://oswaldosantos.github.io/ggsn/
+
+library(ggsn); library(ggplot2); library(sf)
+data("domestic_violence")
+
+(ggm1 <- ggplot(domestic_violence, aes(fill = Scaled)) +
+    geom_sf() +
+    scale_fill_continuous(low = "#fff7ec", high = "#7F0000"))
+
+
+ggm1 +
+  blank() +
+  north(domestic_violence) +
+  scalebar(domestic_violence, dist = 4, dist_unit = "km",
+           transform = TRUE, model = "WGS84")
+
+
+# add north arrow and scale
+p1 + ggspatial::annotation_scale(
+  location = "tr",
+  bar_cols = c("grey60", "white"),
+  text_family = "ArcherPro Book"
+) +
+  ggspatial::annotation_north_arrow(
+    location = "tr", 
+    which_north = "true",
+    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
+    style = ggspatial::north_arrow_nautical(
+      fill = c("grey40", "white"),
+      line_col = "grey20",
+      text_family = "ArcherPro Book"
+    )
+  )
+
+
+
+
+
+
+
+#  Example thematic map raster --------------------------------------------
+
+# https://rdrr.io/cran/tmap/man/tm_raster.html
+
+pal8 <- c("#33A02C", "#B2DF8A", "#FDBF6F", "#1F78B4", "#999999", "#E31A1C", "#E6E6E6", "#A6CEE3")
+
+
+tm_shape(land, ylim = c(-88,88)) +
+  tm_raster("cover_cls", palette = pal8, title = "Global Land Cover") +
+  tm_shape(metro) + tm_dots(col = "#E31A1C") +
+  tm_shape(World) +
+  tm_borders(col = "black") +
+  tm_layout(scale = .8, 
+            legend.position = c("left","bottom"),
+            legend.bg.color = "white", legend.bg.alpha = .2, 
+            legend.frame = "gray50")
+
+
+pal20 <- c("#003200", "#3C9600", "#006E00", "#556E19", "#00C800", "#8CBE8C",
+           "#467864", "#B4E664", "#9BC832", "#EBFF64", "#F06432", "#9132E6",
+           "#E664E6", "#9B82E6", "#B4FEF0", "#646464", "#C8C8C8", "#FF0000",
+           "#FFFFFF", "#5ADCDC")
+tm_shape(land) +
+  tm_raster("cover", palette = pal20, title = "Global Land Cover") + 
+  tm_layout(scale=.8, legend.position = c("left","bottom"))
+
+
+
+
+tm_shape(land, ylim = c(-88,88)) +
+  tm_raster("trees", palette = "Greens", title = "Percent Tree Cover") +
+  tm_shape(World) +
+  tm_borders() +
+  tm_layout(legend.position = c("left", "bottom"), bg.color = "lightblue")
+
+## Not run: 
+tm_shape(land) +
+  tm_raster("black") +
+  tm_facets(by="cover_cls")
+
+
+
